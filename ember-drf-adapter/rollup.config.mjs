9e1501c73617d0b171aa85d061ad43cf5b342c4a
Tommy Carter <tommyjr@gmail.com>
2023-09-07 18:31:38 -0500

    Fixed dependencies, I think

--- ember-drf-adapter/rollup.config.mjs
+++ ember-drf-adapter/rollup.config.mjs
@@ -12,15 +12,21 @@ export default {
   // You can augment this if you need to.
   output: addon.output(),
 
+  external: ['@ember-data'],
+
   plugins: [
     // These are the modules that users should be able to import from your
     // addon. Anything not listed here may get optimized away.
-    addon.publicEntrypoints(['components/**/*.js', 'index.js']),
+    addon.publicEntrypoints([
+      'adapters/**/*.js',
+      'serializers/**/*.js',
+      'index.js',
+    ]),
 
     // These are the modules that should get reexported into the traditional
     // "app" tree. Things in here should also be in publicEntrypoints above, but
     // not everything in publicEntrypoints necessarily needs to go here.
-    addon.appReexports(['components/**/*.js']),
+    addon.appReexports(['adapters/**/*.js', 'serializers/**/*.js']),
 
     // Follow the V2 Addon rules about dependencies. Your code can import from
     // `dependencies` and `peerDependencies` as well as standard Ember-provided
@@ -46,7 +52,7 @@ export default {
 
     // addons are allowed to contain imports of .css files, which we want rollup
     // to leave alone and keep in the published output.
-    addon.keepAssets(['**/*.css']),
+    // addon.keepAssets(['**/*.css']),
 
     // Remove leftover build artifacts when starting a new build.
     addon.clean(),
