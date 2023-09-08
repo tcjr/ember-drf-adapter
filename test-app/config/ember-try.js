48a85876393f1a70a459b3c4a0bad835a56cff36
Tommy Carter <tommyjr@gmail.com>
2023-09-08 09:25:30 -0500

    Added ember-data to ember-try

--- test-app/config/ember-try.js
+++ test-app/config/ember-try.js
@@ -11,6 +11,7 @@ module.exports = async function () {
         name: 'ember-lts-4.8',
         npm: {
           devDependencies: {
+            'ember-data': '~4.8.0',
             'ember-source': '~4.8.0',
           },
         },
@@ -19,6 +20,7 @@ module.exports = async function () {
         name: 'ember-lts-4.12',
         npm: {
           devDependencies: {
+            'ember-data': '~4.12.0',
             'ember-source': '~4.12.0',
           },
         },
@@ -27,6 +29,7 @@ module.exports = async function () {
         name: 'ember-release',
         npm: {
           devDependencies: {
+            'ember-data': 'latest',
             'ember-source': await getChannelURL('release'),
           },
         },
@@ -35,6 +38,7 @@ module.exports = async function () {
         name: 'ember-beta',
         npm: {
           devDependencies: {
+            'ember-data': 'beta',
             'ember-source': await getChannelURL('beta'),
           },
         },
@@ -43,6 +47,7 @@ module.exports = async function () {
         name: 'ember-canary',
         npm: {
           devDependencies: {
+            'ember-data': 'canary',
             'ember-source': await getChannelURL('canary'),
           },
         },
